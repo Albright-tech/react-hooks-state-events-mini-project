6c0b4d5fb20ccfbc08344c0c80d35937b83bd40c
Albright <albright.muchori@student.moringaschool.com>
2023-10-29 11:36:37 +0300

    state events mini

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, filterByCategory}) {
+
+  const [selected, setSelected] = useState('')
+  
+  function handleClick(category) {
+    setSelected(category)
+    filterByCategory(category)
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map( category => <button key={category} className={selected === category ? 'selected' : ''} onClick={() => handleClick(category)}>{category}</button>)}
     </div>
   );
 }
-
 export default CategoryFilter;
